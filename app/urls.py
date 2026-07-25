8ca2a44d8a322309f3d7bd3b6b28806203b48bb9
brunocapistrano <brunokreuch303@gmail.com>
2026-07-25 14:20:13 -0300

    finished

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-
-from genres.views import genre_view
+from genres.views import genre_view, genre_detail_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre-list'),
+    path('genres/', genre_view, name='genre'),
+    path('genres/<int:pk>/', genre_detail_view, name='genre-detail'),
 ]
