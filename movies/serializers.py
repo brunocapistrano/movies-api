85f0f75de6227108112a8c7116b1bdb78ce94a83
brunocapistrano <brunokreuch303@gmail.com>
2026-07-30 22:08:07 -0300

    validade with serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -6,3 +6,7 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta():
         model = Movie
         fields = '__all__'
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('O resumo não pode conter mais de 200 caracteres.')
