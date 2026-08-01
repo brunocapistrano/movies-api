aa6047d73b48dd831e066555a4e5c47fa7acf234
brunocapistrano <brunokreuch303@gmail.com>
2026-08-01 18:37:20 -0300

    tokens

--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,17 @@
 from movies.models import Movie
 from rest_framework import generics
 from movies.serializers import MovieSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     
